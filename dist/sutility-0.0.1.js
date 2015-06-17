a8ec5eb235c272d56eb8250fdaae61cde4c7ab30
snoorghorbani <snoorghorbani@gmail.com>
2015-06-17 12:16:25 +0430

    update

---
+++ dist/sutility-0.0.1.js
@@ -0,0 +1,15 @@
+/**
+ * sutility v0.0.1 - 2015-06-17
+ * Functional Library
+ *
+ * Copyright (c) 2015 soushinas noorghorbani <snoorghorbani@gmail.com>
+ * Licensed MIT
+ */
+;(function(undefined){
+    "use strict";
+if (typeof exports !== 'undefined' && typeof module !== 'undefined' && module.exports) {
+        // exports = module.exports = ProjectName;
+} else {
+        // this.ProjectName = ProjectName;
+}
+}).call(this);
